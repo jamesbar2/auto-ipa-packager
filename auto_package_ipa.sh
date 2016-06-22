cf182127528d6f8e1ec72d30994dd70eece2e950
James Barnett <james@pointblanc.net>
2016-06-21 21:07:57 -0500

    Removed Console Outputs for User Friendly Output
    Removed the output for zip, unzip, and cp commands.  Added user
    friendly output so the user knows what’s going on.

--- auto_package_ipa.sh
+++ auto_package_ipa.sh
@@ -3,6 +3,8 @@
 #original credit for the SwiftSupport and Getting the Developer Directory goes to @bq
 #https://github.com/bq/ipa-packager
 
+#To debug or troubleshoot, remove the >/dev/null or 2>/dev/null from the file
+
 IPA="$1"
 IPADIR=$(dirname "${IPA}")
 TEMPDIR="${IPADIR}/$(cat /dev/urandom | env LC_CTYPE=C tr -cd 'a-z0-9' | head -c 5)"
@@ -23,10 +25,12 @@ if [ ! -d "${DEVELOPER_DIR}" ]; then
 fi
 
 #unzip the IPA so we can re-build the .app
-unzip "${IPA}" -d "${TEMPDIR}"
+echo "Unzipping the IPA archive..."
+unzip "${IPA}" -d "${TEMPDIR}" >/dev/null
 
 #rename the original IPA
 mv "${IPA}" "${IPA/.ipa/-original.ipa}"
+echo "Backed up original IPA to ${IPA/.ipa/-original.ipa}"
 
 APPFILE=$(ls "${TEMPDIR}/Payload/")
 APP="${TEMPDIR}/Payload/${APPFILE}"
@@ -35,16 +39,21 @@ APPFRAMEWORKS="${APP}/Frameworks"
 #add the SwiftSupport requirement
 if [ -d "${APPFRAMEWORKS}" ]; then
 
+	echo "Copying Swift Support Requirements..."
 	SWIFTSUPPORT="${TEMPDIR}/SwiftSupport"
 
     mkdir -p "${SWIFTSUPPORT}"
     for SWIFT_LIB in $(ls -1 "${APPFRAMEWORKS}"); do
-        cp "${DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/${SWIFT_LIB}" "${SWIFTSUPPORT}"
+    	#may throw an error, saying it can't find a *.framework, this is normal
+        cp "${DEVELOPER_DIR}/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/iphoneos/${SWIFT_LIB}" "${SWIFTSUPPORT}" >/dev/null 2>/dev/null
     done
 fi
 
 #zip the files, save the ipa and cleanup
+echo "Zipping updated IPA archive..."
 cd "${TEMPDIR}"
-zip --symlinks --recurse-paths "${IPA}" .
+zip --symlinks --recurse-paths "${IPA}" . >/dev/null
 cd "${IPADIR}"
 rm -d -r "${TEMPDIR}"
+
+echo "Completed! Saved as ${IPA}"
